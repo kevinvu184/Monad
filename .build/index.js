a25dd3402518979616270fca5c2972168bb5dc39
kevinvu184 <kevinvu184@gmail.com>
2023-06-13 00:38:53 +0000

    Create first example

--- .build/index.js
+++ .build/index.js
@@ -1,2 +1,35 @@
 "use strict";
+function wrappedNum(number) {
+  return {
+    number,
+    logs: []
+  };
+}
+function addTwo(number) {
+  return {
+    number: number + 2,
+    logs: [`${number} + 2`]
+  };
+}
+function mulTwo(number) {
+  return {
+    number: number * 2,
+    logs: [`${number} * 2`]
+  };
+}
+function run(numberWithLogs, transform) {
+  const transformed = transform(numberWithLogs.number);
+  return {
+    number: transformed.number,
+    logs: numberWithLogs.logs.concat(transformed.logs)
+  };
+}
+const add = run(wrappedNum(3), addTwo);
+const mul = run(wrappedNum(3), mulTwo);
+const chained = run(run(wrappedNum(3), addTwo), mulTwo);
+const deepChained = run(run(run(wrappedNum(3), addTwo), mulTwo), addTwo);
+console.log(add);
+console.log(mul);
+console.log(chained);
+console.log(deepChained);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,43 @@
-console.log("Hello world!")
+type WithLog = {
+  number: number
+  logs: string[]
+}
+
+function wrappedNum(number: number): WithLog {
+  return {
+    number: number,
+    logs: []
+  }
+}
+
+function addTwo(number: number): WithLog {
+  return {
+    number: number + 2,
+    logs: [`${number} + 2`]
+  }
+}
+
+function mulTwo(number: number): WithLog {
+  return {
+    number: number * 2,
+    logs: [`${number} * 2`]
+  }
+}
+
+function run(numberWithLogs: WithLog, transform: (_: number) => WithLog): WithLog {
+  const transformed = transform(numberWithLogs.number)
+  return {
+    number: transformed.number,
+    logs: numberWithLogs.logs.concat(transformed.logs)
+  }
+}
+
+const add = run(wrappedNum(3), addTwo)
+const mul = run(wrappedNum(3), mulTwo)
+const chained = run(run(wrappedNum(3), addTwo), mulTwo)
+const deepChained = run(run(run(wrappedNum(3), addTwo), mulTwo), addTwo)
+
+console.log(add)
+console.log(mul)
+console.log(chained)
+console.log(deepChained)
